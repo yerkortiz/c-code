35fa29fc8263951be6b735b93fddd9a2d8ac22d9
Yerko <yerko.ortizm@mail.udp.cl>
2019-09-15 23:14:09 -0300

    extended erathostenes

--- number-theory/erathostenes/sieve.c
+++ number-theory/erathostenes/sieve.c
@@ -1,3 +1,9 @@
+/*
+* CON EXTENDED ERATHOSTENES
+* SE PUEDE CALCULAR LOS FACTORES PRIMOS DE
+* UN ENTERO N CUALQUIERA EN COMPLEJIDAD
+* O(LOG(N)).
+*/
 #include <stdio.h>
 #define MAX_SIZE (1 << 8)
 int primes[MAX_SIZE];
@@ -6,6 +12,11 @@ void printPrimes(void)
 {
     for(int *it = (primes + 2), i = 2; it < end; ++it, ++i)
         printf("NÚMERO %d: %s\n", i, (*it) ? "COMPUESTO" : "PRIMO");
+}
+void printMinFactor(void)
+{
+    for(int *it = (primes + 2), i = 2; it < end; ++it, ++i)
+        printf("NÚMERO %d TIENE MÍNIMO FACTOR %d \n", i, *it);
 }
 void erathostenes(int n)
 {
@@ -16,9 +27,20 @@ void erathostenes(int n)
             primes[j] = 1;
     }
 }
+void extendedErathostenes(int n)
+{
+    end += (n + 1);
+    for(int i = 2; i <= n; ++i) {
+        if (primes[i]) continue;
+        for(int j = i << 1; j <= n; j += i)
+            primes[j] = i;
+    }
+}
 int main()
 {
-    erathostenes((1 << 8) - 1);
-    printPrimes();
+    //erathostenes((1 << 8) - 1);
+    //printPrimes();
+    extendedErathostenes((1 << 5) - 1);
+    printMinFactor();
     return 0;
 }
