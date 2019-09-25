342c433d020c0133c37ef4e0bf3b067ad4129db6
Yerko <yerko.ortizm@mail.udp.cl>
2019-09-25 11:22:19 -0300

    asdas

--- operating-systems/syscalls/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "MOVIENDO WEAITAS EN " $target1 "HACIA" $target2
-target1="/Users/yerko/codes/c-code/operating-systems/syscalls/fork"
-target2="/Users/yerko/codes/c-code/operating-systems/syscalls"
-for f in "$target1"/*
-do
-	mv $(basename $f) $target2
-done
