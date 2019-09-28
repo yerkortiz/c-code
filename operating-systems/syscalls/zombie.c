1df77e4286cd4cee90b47dd1eca60801a7491268
Yerko <yerko.ortizm@mail.udp.cl>
2019-09-28 07:58:03 -0300

    zombie bug

--- operating-systems/syscalls/zombie.c
+++ operating-systems/syscalls/zombie.c
@@ -9,14 +9,14 @@ int main(void)
 { 
     pid_t child_pid = fork(); 
     if (child_pid > 0){
-        //system("ps -l");
+        system("ps -l");
         //sleep(5);
         wait(NULL);
         /* kill(pid, SIGKILL); */
         printf("ZOMBIE EXITERMINADO\n");
         system("ps -l");
     } else {
-        printf("ZOMBIE ID %d", getpid());
+        printf("ZOMBIE ID %d\n", getpid());
         exit(0);
     }
     return 0; 
