b27326753d34d8ab9168e9cccf46049f5d6909ee
Yerko <yerko.ortizm@mail.udp.cl>
2019-10-05 18:53:08 -0300

    FILOSOFOS COMPUTINES SOLUCION NO CONCURRENTE

--- operating-systems/sync/cyber-smp.c
+++ operating-systems/sync/cyber-smp.c
@@ -10,19 +10,10 @@
 #define AND &&
 #define LEFT(i, j) (i + MAX_COMPUTER - j) % MAX_COMPUTER
 #define RIGHT(i, j) (i + j) % MAX_COMPUTER
-/* 
- * ESTADO DE UN COMPUTADOR,
- * WAITING = 0 MANOS, BORED = 1 MANO, 
- * PLAYING = 2 MANOS 
-*/
+
 enum {WAITING, BORED, PLAYING} state[MAX_COMPUTER]; 
-int hand_state[MAX_COMPUTER] = {0};
-//int play_ready[MAX_COMPUTER];
-//int current_ready;
-/* 
- * ESTA FUNCION(play) SOLUCIONA EL PROBLEMA DE FORMA 
- * NO CONCURRENTE 
-*/
+int hand_state[MAX_COMPUTER];
+int cnt;
 void printArray(void)
 {
     int i;
@@ -33,7 +24,6 @@ void printArray(void)
 void play(void)
 {
     int i, j;
-    /* Iterar pares y luego impares */
     for(i = 0, j = 0; j < 2 * MAX_COMPUTER; i = RIGHT(i, 2), ++j) {
         /* printf("COMPUTADOR %d NECESITA MANO %d Y MANO %d\n", i, RIGHT(i, 1), LEFT(i, 1)); */
         if(NOT hand_state[i] AND NOT hand_state[LEFT(i, 1)]){
@@ -42,10 +32,20 @@ void play(void)
             printArray();
         } else if(hand_state[LEFT(i, 1)] AND NOT hand_state[i]) {
             printf("COMPUTADOR %d RECIBE SEÑAL DE MANO %d\n", i, i);
-            printArray();
             ++hand_state[i];
+            printArray();
+            ++cnt;
+        } 
+        if(cnt == 2) {
             printf("______________\n");
-            printf("COMPUTADOR %d JUEGA CON COMPUTADOR %d\n")
+            printf("COMPUTADOR %d JUEGA CON COMPUTADOR %d\n", i, LEFT(i, 2));
+            --hand_state[i];
+            --hand_state[LEFT(i, 1)];
+            --hand_state[RIGHT(i, 2)];
+            --hand_state[LEFT(RIGHT(i, 2), 1)];
+            printArray();
+            printf("______________\n");
+            cnt = 0;
         }
     }
 }
